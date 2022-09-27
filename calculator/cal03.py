26eff4877ab6d79ae3c4f57a4db9f14f16a42847
Ramzan Bhatti <mrramzan730@gmail.com>
2022-09-27 22:32:36 +0500

    calculator

--- calculator/cal03.py
+++ calculator/cal03.py
@@ -3,16 +3,16 @@
 def add(num1, num2):
 	return num1 + num2
 
-def sub(num1, num2)
+def sub(num1, num2):
 	Return num1 - num2
 
-def mul(num1, num2)
+def mul(num1, num2):
         return num1 * num2
 
-def div(num1, num2)
+def div(num1, num2):
         return num1/num2
 
-def sqroot(num1, num2)
+def sqroot(num1, num2):
         return num1**num2
 
 
