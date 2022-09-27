84791cad075bf6c767e86438184896284cb90e83
Sandeep Shivani <sandeep2020@namal.edu.pk>
2022-09-27 22:00:25 +0500

    cal03

---
+++ calculator/cal03.py
@@ -0,0 +1,25 @@
+# calculator in Python(v0.3)
+
+def add(num1, num2):
+	return num1 + num2
+
+def sub(num1, num2)
+	Return num1 - num2
+
+def mul(num1, num2)
+        return num1 * num2
+
+def div(num1, num2)
+        return num1/num2
+
+def sqroot(num1, num2)
+        return num1**num2
+
+
+# main
+res = add(5, 3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+print(div(5,3))
+print(sqroot(5,3))
